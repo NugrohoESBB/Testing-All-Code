0b6e4593e3adf71c0aa1be81a0e871eb14889747
Nugroho Eko S Batubara <89783476+NugrohoESBB@users.noreply.github.com>
2025-03-05 13:10:03 +0700

    update

--- ImageStream/stream.py
+++ ImageStream/stream.py
@@ -1,9 +1,9 @@
 #include <WiFi.h>
 #include <ThingSpeak.h>
 #include <Adafruit_Sensor.h>
-#include <DHT.h>
 #include <LiquidCrystal_I2C.h>
 #include <elapsedMillis.h>
+#include <DHT.h>
 
 #define DHTPIN 4
 #define DHTTYPE DHT21
@@ -18,55 +18,72 @@ const char* password          = "Gepuk15000";
 const char* writeAPIKey       = "X0076DPBF0F1X2OE";
 const unsigned long channelID = 2847979;
 
-unsigned long thingSpeakInterval  = 15000;
-unsigned long sensorInterval      = 500;
-unsigned long displayInterval     = 1000;
+int soilValue, APIhandler;
+float soilPercentage, temperature, humidity;
+
+unsigned long thingSpeakInterval = 15000, sensorInterval = 500, lcdInterval = 2000, serialInterval = 2000;
 
 WiFiClient client;
-
 DHT dht(DHTPIN, DHTTYPE);
-elapsedMillis thingSpeakMillis;
-elapsedMillis sensorMillis;
-elapsedMillis displayMillis;
+elapsedMillis thingSpeakMillis, sensorMillis, lcdMillis, serialMillis;
 LiquidCrystal_I2C lcd(0x27, 20, 4);
-
-int soilValue;
-float soilPercentage;
-float temperature;
-float humidity;
 
 void setup() {
   Serial.begin(115200);
-
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(3, 0);
-  lcd.print("Selamat Datang!");
-  lcd.setCursor(0, 1);
-  lcd.print("-");
-  lcd.setCursor(3, 3);
-  lcd.print("-- UG MURO --");
-  delay(5000);
-  lcd.clear();
-  delay(2000);
 
   pinMode(soilPin, INPUT);
   pinMode(RELAY1_PIN, OUTPUT);
   pinMode(RELAY2_PIN, OUTPUT);
   pinMode(RELAY3_PIN, OUTPUT);
   pinMode(RELAY4_PIN, OUTPUT);
-  dht.begin();
 
+  Wire.begin();
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print("   INITIATION....   ");
+
+  delay (1000);
+
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println(".");
+    delay(500);
+    Serial.print(".");
+    lcd.setCursor(0, 1);
+    lcd.print("   Connecting....   ");
   }
-  Serial.println("Connected to WiFi");
+
+  lcd.setCursor(0, 1);
+  lcd.print("                    ");
+  lcd.setCursor(0, 2);
+  lcd.print("  WiFi: " + String (ssid));
+
+  delay(1000);
+
+  dht.begin();
   ThingSpeak.begin(client);
+
+  lcd.setCursor(0, 3);
+  lcd.print("                    ");
+  lcd.setCursor(0, 3);
+  lcd.print("     ALL CLEAR!     ");
+  delay(2000);
+  lcd.clear();
 }
 
 void loop() {
+  if (serialMillis >= serialInterval) {
+    serialOutput();
+    serialMillis = 0;
+  }
+
+  if (lcdMillis >= lcdInterval) {
+    lcdOutput()
+    lcdMillis = 0;
+  }
+
   if (sensorMillis >= sensorInterval) {
     temperature = dht.readTemperature();
     humidity = dht.readHumidity();
@@ -74,32 +91,9 @@ void loop() {
     soilValue = analogRead(soilPin);
     soilPercentage = map(soilValue, 4095, 0, 0, 100);
     soilPercentage = constrain(soilPercentage, 0, 100);
-
-    Serial.print("Humidity: ");
-    Serial.print(humidity);
-    Serial.print("% \t");
-    Serial.print("Temperature : ");
-    Serial.print(temperature);
-    Serial.print("*C \t");
-    Serial.print("Soil: ");
-    Serial.print(soilPercentage);
-    Serial.println("%");
+    conditionRelay();
 
     sensorMillis = 0;
-  }
-
-  if (displayMillis >= displayInterval) {
-    lcd.setCursor(0, 0);
-    lcd.print("Monitoring");
-
-    lcd.setCursor(0, 1);
-    lcd.print("Suhu: " + String(temperature) + "°C");
-    lcd.setCursor(0, 2);
-    lcd.print("Hum: " + String(humidity) + "%");
-    lcd.setCursor(0, 3);
-    lcd.print("Soil: " + String(soilPercentage) + "%");
-
-    displayMillis = 0;
   }
 
   if (thingSpeakMillis >= thingSpeakInterval) {
@@ -107,30 +101,43 @@ void loop() {
     ThingSpeak.setField(2, humidity);
     ThingSpeak.setField(3, soilPercentage);
 
-    int x = ThingSpeak.writeFields(channelID, writeAPIKey);
-    if (x == 200) {
+    APIhandler = ThingSpeak.writeFields(channelID, writeAPIKey);
+    if (APIhandler == 200) {
       Serial.println("Update successful.");
     } else {
-      Serial.println("Update failed. HTTP error code: " + String(x));
+      Serial.println("Update failed. HTTP error code: " + String(APIhandler));
     }
     thingSpeakMillis = 0;
   }
 }
 
+void lcdOutput() {
+  lcd.setCursor(0, 0);
+  lcd.print("Monitoring");
+  lcd.setCursor(0, 1);
+  lcd.print("Suhu: " + String(temperature) + "°C");
+  lcd.setCursor(0, 2);
+  lcd.print("Hum: " + String(humidity) + "%");
+  lcd.setCursor(0, 3);
+  lcd.print("Soil: " + String(soilPercentage) + "%");
+}
+
+void serialOutput() {
+  Serial.println("Temperature : " + String(temperature) + "*C");
+  Serial.println("Humidity    : " + String(humidity) + "%");
+  Serial.println("Soil        : " + String(soilPercentage) + "%");
+}
+
 void conditionRelay() {
-  if (temperature > 20) {
+  if (temperature > 20.0) {
     digitalWrite(RELAY1_PIN, LOW);
-    digitalWrite(RELAY2_PIN, LOW);
   } else {
     digitalWrite(RELAY1_PIN, HIGH);
-    digitalWrite(RELAY2_PIN, HIGH);
   }
 
-  if (soilPercentage > 20) {
+  if (soilPercentage > 50.0) {
     digitalWrite(RELAY3_PIN, LOW);
-    digitalWrite(RELAY4_PIN, LOW);
   } else {
     digitalWrite(RELAY3_PIN, HIGH);
-    digitalWrite(RELAY4_PIN, HIGH);
   }
 }
